3edc9bea21cd269c89c23d96fd6a8618e347314a
Elena M <LenaHelen17@gmail.com>
2021-10-24 20:03:57 +0300

    JdbcMealRepository implement via Spring JDBC Template

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,8 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
+DELETE  FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +12,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2021-10-23 10:28:08', 'Завтрак', 500, 100000),
+       ('2021-10-23 13:32:00', 'Обед', 730, 100000),
+       ('2021-10-23 13:32:00', 'Обед', 730, 100001),
+       ('2021-10-25 20:00:00', 'Ужин', 600, 100000);
